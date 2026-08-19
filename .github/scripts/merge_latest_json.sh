127d2d70ebf441167625c566dca51b7ad634222d
an-lee <an.lee.work@gmail.com>
2026-08-20 07:15:57 +0800

    Revert "fix(release): merge latest.json assets across version bumps"
    This reverts commit 95289d75915041bc6035e997b41c800a4f0c1d34.

--- .github/scripts/merge_latest_json.sh
+++ .github/scripts/merge_latest_json.sh
@@ -23,7 +23,7 @@ fi
 
 jq -s '
   .[0] as $new | .[1] as $remote |
-  if ($remote | type) != "object" then
+  if ($remote | type) != "object" or ($remote.version != $new.version) then
     $new
   else
     $new
